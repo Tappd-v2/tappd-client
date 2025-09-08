d62c17c88bd34efccbf89578294704e40aee1ddc
Lukas Olivier <olivier.lukas2003@gmail.com>
2025-09-08 20:00:04 +0200

    style: new icon

--- app.vue
+++ app.vue
@@ -2,12 +2,12 @@
 const layout = useState('layout', () => 'default');
 
 const description =
-   "Tappd is an online ordering service for restaurants, bars, and cafes.";
+   "Tappd is een innovatieve app voor het bestellen van drankjes.";
 const ogTitle = "Tappd";
 const twitterDescription =
-   "Tappd is an online ordering service for restaurants, bars, and cafes.";
+   "Tappd is een innovatieve app voor het bestellen van drankjes.";
 const image =
-   "https://images.unsplash.com/photo-1608270586620-248524c67de9?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmVlcnxlbnwwfHwwfHx8MA%3D%3D";
+   "https://tappd-demo.lukasolivier.be/og-image.png";
 const mySite = "https://tappd-demo.lukasolivier.be";
 
 useHead({
